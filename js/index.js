5543725258e54e952896891a2170c201b6f2f70c
devdong9897 <dev.dong9897@gmail.com>
2023-05-11 13:13:11 +0900

    update: UI개선

--- js/index.js
+++ js/index.js
@@ -1,20 +1,21 @@
 // html, css, js, image, font, video...
-// 사용되는 리소스가 모드 로드가 완료되고 나서
-// js를 실행하여야 정상적인 처리가 가능하다.
-window.onload = function() {
+// 사용되는 리소스가 모두 로드가 완료되고 나서 형
+// js 를 실행하여야 정상적인 처리가 가능하다.
+
+window.onload = function () {
   // 위로 이동하기
-  // .gotop을 js에 저장하자.
+  // .gotop 을 js에 저장하자.
   const goTop = document.querySelector(".gotop");
   // goTop 클릭을 처리한다.
-  goTop.addEventListener("click", function(){
-    // 위로 슬라이드 코드
+  goTop.addEventListener("click", function () {
+    // 위로 슬라이등 코드
     window.scrollTo({
       top: 0,
       behavior: "smooth",
     });
   });
 
-// <!-- Initialize Swiper -->
+  // <!-- Initialize Swiper -->
   let promotionSwiper = new Swiper(".sw-promotion", {
     slidesPerView: 1,
     spaceBetween: 24,
@@ -39,129 +40,153 @@ window.onload = function() {
     },
   });
 
-//   <!-- Shopping Swiper -->
+  // <!-- Shopping Swiper -->
 
-    let shoppingSwiper = new Swiper(".sw-shopping", {
+  let shoppingSwiper = new Swiper(".sw-shopping", {
     slidesPerView: 5,
     grid: {
-        rows: 2,
-        fill: "row",
+      rows: 2,
+      fill: "row",
     },
     spaceBetween: 10,
+    navigation: {
+      nextEl: ".shopping .sw-next",
+      prevEl: ".shopping .sw-prev",
+    },
     breakpoints: {
-        1024: {
+      1024: {
         spaceBetween: 32,
         slidesPerView: 3,
         // 화면당 3개씩 슬라이드 이동
         slidesPerGroup: 3,
         grid: {
-            rows: 1,
+          rows: 1,
         },
-        },
-        1280: {
+      },
+      1280: {
         spaceBetween: 26,
         slidesPerView: 4,
         // 화면당 4개씩 슬라이드 이동
         slidesPerGroup: 4,
         grid: {
-            rows: 1,
+          rows: 1,
         },
-        },
+      },
     },
-    });
-              
-    let tourSwiper = new Swiper(".sw-tour", {
+  });
+
+  let tourSwiper = new Swiper(".sw-tour", {
     slidesPerView: 3,
     grid: {
-        rows: 2,
-        fill: "row",
+      rows: 2,
+      fill: "row",
     },
     spaceBetween: 10,
+    navigation: {
+      nextEl: ".tour .sw-next",
+      prevEl: ".tour .sw-prev",
+    },
     breakpoints: {
-        1024: {
-        spaceBetween: 24,
+      1024: {
+        spaceBetween: 32,
         slidesPerView: 2,
         // 화면당 2개씩 슬라이드 이동
         slidesPerGroup: 2,
         grid: {
-            rows: 1,
+          rows: 1,
         },
-        },
-        1280: {
+      },
+      1280: {
         spaceBetween: 26,
         slidesPerView: 3,
         // 화면당 4개씩 슬라이드 이동
         slidesPerGroup: 3,
         grid: {
-            rows: 1,
+          rows: 1,
         },
-        },
+      },
     },
-    });
+  });
 
-    let tickerSwiper = new Swiper(".sw-ticket", {
+  let ticketSwiper = new Swiper(".sw-ticket", {
     slidesPerView: "auto",
     spaceBetween: 10,
+    navigation: {
+      nextEl: ".ticket .sw-next",
+      prevEl: ".ticket .sw-prev",
+    },
     breakpoints: {
-        1024: {
+      1024: {
         slidesPerView: 3,
         spaceBetween: 32,
-        },
-        1280: {
+      },
+      1280: {
         slidesPerView: 4,
         spaceBetween: 27,
-        },
+      },
     },
-    });
-    let liveSwiper = new Swiper(".sw-live", {
+  });
+
+  let liveSwiper = new Swiper(".sw-live", {
     slidesPerView: 4,
     spaceBetween: 10,
-    breakpoints : {
-        1024: {
+    navigation: {
+      nextEl: ".live .sw-next",
+      prevEl: ".live .sw-prev",
+    },
+    breakpoints: {
+      1024: {
         slidesPerView: 3,
         spaceBetween: 32,
-        },
-        1280: {
+      },
+      1280: {
         slidesPerView: 4,
         spaceBetween: 27,
-        }
-    }
-    });
-    let boosSwiper = new Swiper(".sw-books", {
+      },
+    },
+  });
+  let booksSwiper = new Swiper(".sw-books", {
     slidesPerView: 3,
     grid: {
-        rows: 4,
-        fill: "row",
+      rows: 4,
+      fill: "row",
     },
     spaceBetween: 19,
+    navigation: {
+      nextEl: ".books .sw-next",
+      prevEl: ".books .sw-prev",
+    },
     breakpoints: {
-        1024: {
+      1024: {
         slidesPerView: 3,
         slidesPerGroup: 3,
         spaceBetween: 30,
         grid: {
-        rows: 1,
+          rows: 1,
         },
-        },
-        1280: {
+      },
+      1280: {
         slidesPerView: 5,
         slidesPerGroup: 5,
         spaceBetween: 27,
         grid: {
-        rows: 1,
-        fill: "row",
+          rows: 1,
         },
-        },
+      },
     },
-    });
+  });
 
-    let eventSwiper = new Swiper(".sw-events", {
+  let eventsSwiper = new Swiper(".sw-events", {
     slidesPerView: 3,
     spaceBetween: 27,
-    breakpoints: {
-        1280: {
-        slidesPerView: 4,
-        },
+    navigation: {
+      nextEl: ".event .sw-next",
+      prevEl: ".event .sw-prev",
     },
-    });
-  }
+    breakpoints: {
+      1280: {
+        slidesPerView: 4,
+      },
+    },
+  });
+};
